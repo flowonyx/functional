4f51c1c609447cfbad5af4e88409c60c0ab596c0
Joel Williams <flowonyx@gmail.com>
2022-02-23 13:49:29 +0200

    Added a curry function Curry2To1F

--- curry.go
+++ curry.go
@@ -24,6 +24,14 @@ func Curry2To1[T1, T2, T3 any](f func(T1, T2) T3, input1 T1) func(T2) T3 {
 	}
 }
 
+func Curry2To1F[T1, T2, T3 any](f func(T1, T2) T3) func(T1) func(T2) T3 {
+	return func(input1 T1) func(T2) T3 {
+		return func(input2 T2) T3 {
+			return f(input1, input2)
+		}
+	}
+}
+
 func Curry3To2[T1, T2, T3, T4 any](f func(T1, T2, T3) T4, input1 T1) func(T2, T3) T4 {
 	return func(input2 T2, input3 T3) T4 {
 		return f(input1, input2, input3)
