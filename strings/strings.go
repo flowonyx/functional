3c65760ff71885723a388f436432b3988c241cf5
Joel Williams <flowonyx@gmail.com>
2022-02-23 13:52:37 +0200

    Renamed package:fixed bug:added more funcs

--- strings/strings.go
+++ strings/strings.go
@@ -1,4 +1,4 @@
-package string
+package strings
 
 import (
 	"strings"
@@ -43,7 +43,7 @@ func ForAll(predicate functional.Predicate[rune], str string) bool {
 
 func InitString(count int, initializer func(int) string) string {
 	output := &strings.Builder{}
-	list.DoRangeTo(func(i int) { output.WriteString(initializer(i)) }, count)
+	list.DoRangeTo(func(i int) { output.WriteString(initializer(i)) }, count-1)
 	return output.String()
 }
 
@@ -63,8 +63,36 @@ func Map(mapping functional.Projection[rune, rune], str string) string {
 	return strings.Map(mapping, str)
 }
 
+func MapTo[T any](mapping functional.Projection[rune, T], str string) []T {
+	output := make([]T, len(str))
+	for i, r := range str {
+		output[i] = mapping(r)
+	}
+	return output
+}
+
 func Mapi(mapping func(int, rune) rune, str string) string {
 	output := &strings.Builder{}
 	Iteri(func(i int, r rune) { output.WriteRune(mapping(i, r)) }, str)
 	return output.String()
+}
+
+func HasPrefix[T ~string | ~rune](s string, prefix T) bool {
+	return strings.HasPrefix(s, string(prefix))
+}
+
+func HasSuffix[T ~string | ~rune](s string, suffix T) bool {
+	return strings.HasSuffix(s, string(suffix))
+}
+
+func Range(start, end rune) []rune {
+	r := []rune{}
+	for i := int(start); i <= int(end); i++ {
+		r = append(r, rune(i))
+	}
+	return r
+}
+
+func FromRunes(input []rune) string {
+	return InitString(len(input), func(i int) string { return string(input[i]) })
 }
