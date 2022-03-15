f93eae126f69a61cd4df470ac05ea6b014f0bda8
Joel Williams <flowonyx@gmail.com>
2022-03-16 00:51:24 +0200

    Added testing to set

--- set/set.go
+++ set/set.go
@@ -1,20 +1,33 @@
+// Package set provides a generic Set type.
 package set
 
 import (
 	"github.com/flowonyx/functional"
-	. "github.com/flowonyx/functional/list"
+	"github.com/flowonyx/functional/list"
 	"github.com/flowonyx/functional/orderedMap"
 	"golang.org/x/exp/constraints"
 )
 
+// Set is a type keeps a set of values and allows set operations on them.
 type Set[T comparable] struct {
 	m orderedMap.OrderedMap[T, struct{}]
 }
 
+// NewSet creates a new Set.
+// If lessFunc is provided, it is used in ordering the set.
 func NewSet[T comparable](lessFunc ...func(T, T) bool) Set[T] {
-	return Set[T]{m: orderedMap.NewOrderedMap[T, struct{}](lessFunc...)}
+	var lf func(functional.Pair[T, struct{}], functional.Pair[T, struct{}]) bool
+	if len(lessFunc) > 0 {
+		lf = func(p functional.Pair[T, struct{}], p2 functional.Pair[T, struct{}]) bool {
+			return lessFunc[0](p.First, p2.First)
+		}
+	}
+	return Set[T]{m: orderedMap.NewOrderedMap(lf)}
 }
 
+// Singleton creates a set of exactly one item.
+// This does not actually prevent you from adding more items later,
+// so this is a way to creat a set and add the first item in one call.
 func Singleton[T comparable](item T) Set[T] {
 	s := NewSet[T]()
 	s.Add(item)
@@ -29,46 +42,59 @@ func (s Set[T]) clone() Set[T] {
 	return n
 }
 
-func FromSlice[T comparable](input []T) Set[T] {
-	s := NewSet[T]()
+// FromSlice creates a new Set from the items in the given slice.
+func FromSlice[T comparable](input []T, lessFunc ...func(T, T) bool) Set[T] {
+	s := NewSet(lessFunc...)
 	for _, i := range input {
 		s.Add(i)
 	}
 	return s
 }
 
+// ToSlice returns the values in the Set as a slice of items.
 func (s Set[T]) ToSlice() []T {
 	return s.Items()
 }
 
-func (s Set[T]) Add(item T) {
+// Add adds an item to the Set. If it already exists in the set, nothing changes.
+func (s *Set[T]) Add(item T) {
 	s.m.Set(item, struct{}{})
 }
 
-func (s Set[T]) Remove(item T) {
+// Remove removes an item from the Set.
+func (s *Set[T]) Remove(item T) {
 	s.m.Remove(item)
 }
 
+// Equal tests if two sets are equal.
 func (s Set[T]) Equal(s2 Set[T]) bool {
 	return orderedMap.Equal(s.m, s2.m)
 }
 
+// Contains tests whether item is present in the Set.
 func (s Set[T]) Contains(item T) bool {
 	return s.m.Contains(item)
 }
 
-func (s Set[T]) Exists(predicate functional.Predicate[T]) bool {
-	return s.m.Exists(func(item T, _ struct{}) bool { return predicate(item) })
+// Exists tests whether any item in the Set matches the predicate.
+func (s Set[T]) Exists(predicate func(T) bool) bool {
+	return list.Exists(predicate, s.Items())
 }
 
+// Count returns the number of items in the Set.
 func (s Set[T]) Count() int {
 	return s.m.Len()
 }
 
+// IsEmpty test whether this is an empty set.
 func (s Set[T]) IsEmpty() bool {
 	return s.Count() == 0
 }
 
+// IsSubsetOf tests whether this Set is a subset of the Set passed as a parameter.
+// A Set is a subset of another Set if it only contains items that are also present in the other Set.
+// An empty Set is considered a subset of any other Set. This also considers equal sets to be subsets of each other.
+// If you do not want these cases to be considered a subset, use IsProperSubsetOf instead.
 func (s Set[T]) IsSubsetOf(potentialSuperset Set[T]) bool {
 	if potentialSuperset.Count() < s.Count() {
 		return false
@@ -81,8 +107,12 @@ func (s Set[T]) IsSubsetOf(potentialSuperset Set[T]) bool {
 	return true
 }
 
+// IsProperSubsetOf tests whether this Set is a subset of the Set passed as a parameter.
+// A Set is a subset of another Set if it only contains items that are also present in the other Set.
+// An empty Set is not considered a subset of any other Set. Also, if the Sets are equal, neither is considered
+// to a subset of the other. If you do want these cases to be considered a subset, use IsSubsetOf instead.
 func (s Set[T]) IsProperSubsetOf(potentialSuperset Set[T]) bool {
-	if potentialSuperset.Count() == 0 || s.Count() == 0 {
+	if potentialSuperset.IsEmpty() || s.IsEmpty() {
 		return false
 	}
 	if potentialSuperset.Count() <= s.Count() {
@@ -96,14 +126,24 @@ func (s Set[T]) IsProperSubsetOf(potentialSuperset Set[T]) bool {
 	return true
 }
 
+// IsProperSupersetOf tests whether this Set is a superset of the Set passed as a parameter.
+// A Set is a superset of another Set if it contains all items that are present in the other Set.
+// If either Set is empty or if they are equal, this will not consider the Set to be a superset.
+// If you want these cases to be considered as supersets, use IsSupersetOf instead.
 func (s Set[T]) IsProperSupersetOf(potentialSubset Set[T]) bool {
 	return potentialSubset.IsProperSubsetOf(s)
 }
 
+// IsSupersetOf tests whether this Set is a superset of the Set passed as a parameter.
+// A Set is a superset of another Set if it contains all items that are present in the other Set.
+// If the other Set is empty or if they are equal, this will consider the Set to be a superset.
+// If you do not want these cases to be considered as supersets, use IsProperSupersetOf instead.
 func (s Set[T]) IsSupersetOf(potentialSubset Set[T]) bool {
 	return potentialSubset.IsSubsetOf(s)
 }
 
+// IndexOf finds the index of the item within the Set.
+// The order of items within the Set is the order in which the items were added.
 func (s Set[T]) IndexOf(item T) int {
 	index := -1
 	s.m.Iteri(func(i int, t T, _ struct{}) {
@@ -114,10 +154,12 @@ func (s Set[T]) IndexOf(item T) int {
 	return index
 }
 
+// Items returns the items in the Set as a slice.
 func (s Set[T]) Items() []T {
 	return s.m.Keys()
 }
 
+// Difference returns a Set containing the items that are only present in one Set or the other.
 func (s Set[T]) Difference(set2 Set[T]) Set[T] {
 	output := NewSet[T]()
 	for _, i := range s.Items() {
@@ -133,6 +175,7 @@ func (s Set[T]) Difference(set2 Set[T]) Set[T] {
 	return output
 }
 
+// Intersect returns a Set containing the items that are present in both Sets.
 func (s Set[T]) Intersect(set2 Set[T]) Set[T] {
 	output := NewSet[T]()
 	for _, i := range s.Items() {
@@ -148,6 +191,7 @@ func (s Set[T]) Intersect(set2 Set[T]) Set[T] {
 	return output
 }
 
+// Union returns a Set that contains all items that are present in either Set.
 func (s Set[T]) Union(set2 Set[T]) Set[T] {
 	output := NewSet[T]()
 	for _, i := range s.Items() {
@@ -159,7 +203,8 @@ func (s Set[T]) Union(set2 Set[T]) Set[T] {
 	return output
 }
 
-func (s Set[T]) Filter(predicate functional.Predicate[T]) Set[T] {
+// Filter returns a Set that contains only items from this Set that match the predicate.
+func (s Set[T]) Filter(predicate func(T) bool) Set[T] {
 	output := NewSet[T]()
 	for _, i := range s.Items() {
 		if predicate(i) {
@@ -169,47 +214,53 @@ func (s Set[T]) Filter(predicate functional.Predicate[T]) Set[T] {
 	return output
 }
 
-func (s Set[T]) ForAll(predicate functional.Predicate[T]) bool {
-	return s.m.ForAll(func(k T, _ struct{}) bool { return predicate(k) })
+// ForAll tests whether all items in the set match the predicate.
+func (s Set[T]) ForAll(predicate func(T) bool) bool {
+	return list.ForAll(predicate, s.Items())
 }
 
+// Iter applies the action to each item in the Set.
 func (s Set[T]) Iter(action func(T)) {
 	for _, i := range s.Items() {
 		action(i)
 	}
 }
 
+// Iteri applies the action to each item in the Set,
+// also passing the index to the action.
 func (s Set[T]) Iteri(action func(int, T)) {
 	for i, v := range s.Items() {
 		action(i, v)
 	}
 }
 
-func (s Set[T]) Partition(predicate functional.Predicate[T]) (Set[T], Set[T]) {
-	tset := NewSet[T]()
-	fset := NewSet[T]()
+// Partition returns two Sets. The first Set are those items in this Set
+// that match the predicate. The second Set are those items in this Set
+// that do not match the predicate.
+func (s Set[T]) Partition(predicate func(T) bool) (trueSet Set[T], falseSet Set[T]) {
+	trueSet = NewSet[T]()
+	falseSet = NewSet[T]()
 	s.Iter(func(t T) {
 		if predicate(t) {
-			tset.Add(t)
+			trueSet.Add(t)
 		} else {
-			fset.Add(t)
+			falseSet.Add(t)
 		}
 	})
-	return tset, fset
+	return trueSet, falseSet
 }
 
+// Fold applies the folder function to each item in the Set until it reaches the final state.
 func Fold[T comparable, State any](folder func(State, T) State, initial State, s Set[T]) State {
-	return orderedMap.Fold(func(s State, k T, _ struct{}) State {
-		return folder(s, k)
-	}, initial, s.m)
+	return list.Fold(folder, initial, s.Items())
 }
 
+// FoldBack applies the folder function in reverse order to each item in the Set until it reaches the final state.
 func FoldBack[T comparable, State any](folder func(T, State) State, s Set[T], initial State) State {
-	return orderedMap.FoldBack(func(k T, _ struct{}, s State) State {
-		return folder(k, s)
-	}, s.m, initial)
+	return list.FoldBack(folder, s.Items(), initial)
 }
 
+// DifferenceMany finds the difference between all the sets.
 func DifferenceMany[T comparable](sets ...Set[T]) Set[T] {
 	if len(sets) == 0 {
 		return NewSet[T]()
@@ -221,6 +272,7 @@ func DifferenceMany[T comparable](sets ...Set[T]) Set[T] {
 	return output
 }
 
+// ItersectMany finds the intersection between all the sets.
 func IntersectMany[T comparable](sets ...Set[T]) Set[T] {
 	if len(sets) == 0 {
 		return NewSet[T]()
@@ -232,6 +284,7 @@ func IntersectMany[T comparable](sets ...Set[T]) Set[T] {
 	return output
 }
 
+// UnionMany finds the union between all the sets.
 func UnionMany[T comparable](sets ...Set[T]) Set[T] {
 	if len(sets) == 0 {
 		return NewSet[T]()
@@ -243,24 +296,29 @@ func UnionMany[T comparable](sets ...Set[T]) Set[T] {
 	return output
 }
 
-func Map[T comparable, R comparable](mapping functional.Projection[T, R], s Set[T]) Set[R] {
+// Map applies the mapping function to each item in the Set s.
+func Map[T comparable, R comparable](mapping func(T) R, s Set[T]) Set[R] {
 	output := NewSet[R]()
 	s.Iter(func(t T) { output.Add(mapping(t)) })
 	return output
 }
 
+// MaxElement finds the largest item in the Set s.
 func MaxElement[T constraints.Ordered](s Set[T]) T {
-	return Max(s.Items()...)
+	return list.Max(s.Items()...)
 }
 
+// MinElement finds the smallest item in the Set s.
 func MinElement[T constraints.Ordered](s Set[T]) T {
-	return Min(s.Items()...)
+	return list.Min(s.Items()...)
 }
 
-func MaxElementBy[T comparable, R constraints.Ordered](projection functional.Projection[T, R], s Set[T]) T {
-	return MaxBy(projection, s.Items())
+// MaxElementBy finds the largest item in the Set s using the return values from projection for comparison.
+func MaxElementBy[T comparable, R constraints.Ordered](projection func(T) R, s Set[T]) T {
+	return list.MaxBy(projection, s.Items())
 }
 
-func MinElementBy[T comparable, R constraints.Ordered](projection functional.Projection[T, R], s Set[T]) T {
-	return MinBy(projection, s.Items())
+// MinElementBy finds the smallest item in the Set s using the return values from projection for comparison.
+func MinElementBy[T comparable, R constraints.Ordered](projection func(T) R, s Set[T]) T {
+	return list.MinBy(projection, s.Items())
 }
---
+++ set/set_test.go
@@ -0,0 +1,213 @@
+package set
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func ExampleNewSet() {
+	less := func(a, b int) bool { return a > b }
+	s := NewSet(less)
+	s.Add(1)
+	s.Add(2)
+	s.Add(4)
+	s.Add(3)
+	fmt.Println(s.Items())
+	// Output: [4 3 2 1]
+}
+
+func ExampleSingleton() {
+	s := Singleton(1)
+	fmt.Println(s.Items())
+	// Output: [1]
+}
+
+func ExampleFromSlice() {
+	s := FromSlice([]string{"one", "two", "one"})
+	s2 := FromSlice([]int{1, 3, 4, 2, 4, 3, 2}, func(a, b int) bool { return a < b })
+	fmt.Println(s.Items(), s2.Items())
+	// Output: [one two] [1 2 3 4]
+}
+
+func ExampleSet_Equal() {
+	s := FromSlice([]string{"one", "two", "one"})
+	s2 := FromSlice([]string{"two", "one", "one"})
+	fmt.Println(s.Equal(s2))
+	// Output: true
+}
+
+func ExampleSet_Contains() {
+	s := FromSlice([]string{"one", "two", "one"})
+	fmt.Println(s.Contains("two"), s.Contains("three"))
+	// Output: true false
+}
+
+func ExampleSet_Exists() {
+	s := FromSlice([]string{"one", "two", "one"})
+	r := s.Exists(func(s string) bool { return strings.HasPrefix(s, "o") })
+	r2 := s.Exists(func(s string) bool { return strings.HasPrefix(s, "s") })
+	fmt.Println(r, r2)
+	// Output: true false
+}
+
+func ExampleSet_IsSubsetOf() {
+	s := FromSlice([]string{"one", "two", "one"})
+	s2 := FromSlice([]string{"one", "two", "one", "three"})
+	fmt.Println(s.IsSubsetOf(s2), s2.IsSubsetOf(s))
+	// Output: true false
+}
+
+func ExampleSet_IsSupersetOf() {
+	s := FromSlice([]string{"one", "two", "one"})
+	s2 := FromSlice([]string{"one", "two", "one", "three"})
+	fmt.Println(s.IsSupersetOf(s2), s2.IsSupersetOf(s))
+	// Output: false true
+}
+
+func ExampleSet_IsProperSubsetOf() {
+	s := FromSlice([]string{"one", "two", "one"})
+	s2 := FromSlice([]string{"one", "two", "one", "three"})
+	s3 := FromSlice([]string{"one", "two", "one", "three"})
+	fmt.Println(s.IsProperSubsetOf(s2), s2.IsProperSubsetOf(s), s2.IsProperSubsetOf(s3))
+	// Output: true false false
+}
+
+func ExampleSet_IsProperSupersetOf() {
+	s := FromSlice([]string{"one", "two", "one"})
+	s2 := FromSlice([]string{"one", "two", "one", "three"})
+	s3 := FromSlice([]string{"one", "two", "one", "three"})
+	fmt.Println(s.IsProperSupersetOf(s2), s2.IsProperSupersetOf(s), s2.IsProperSupersetOf(s3))
+	// Output: false true false
+}
+
+func ExampleSet_IndexOf() {
+	s := NewSet(func(a, b int) bool { return a < b })
+	s.Add(1)
+	s.Add(3)
+	s.Add(2)
+	fmt.Println(s.IndexOf(3))
+	// Output: 2
+}
+
+func ExampleSet_Difference() {
+	s := FromSlice([]string{"one", "two", "one"})
+	s2 := FromSlice([]string{"one", "two", "one", "three"})
+	r := s.Difference(s2)
+	fmt.Println(r.Items())
+	// Output: [three]
+}
+
+func ExampleSet_Union() {
+	s := FromSlice([]string{"one", "two", "one"})
+	s2 := FromSlice([]string{"one", "three"})
+	r := s.Union(s2)
+	fmt.Println(r.Items())
+	// Output: [one two three]
+}
+
+func ExampleSet_Intersect() {
+	s := FromSlice([]string{"one", "two", "one"})
+	s2 := FromSlice([]string{"one", "three"})
+	r := s.Intersect(s2)
+	fmt.Println(r.Items())
+	// Output: [one]
+}
+
+func ExampleSet_Filter() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	r := s.Filter(func(i int) bool { return i%2 == 0 })
+	fmt.Println(r.Items())
+	// Output: [2 4]
+}
+
+func ExampleSet_Iter() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	s.Iter(func(item int) { fmt.Print(item) })
+	// Output: 1234
+}
+
+func ExampleSet_Iteri() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	s.Iteri(func(i, item int) { fmt.Print(i, item) })
+	// Output: 0 11 22 33 4
+}
+
+func ExampleSet_Partition() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	t, f := s.Partition(func(i int) bool { return i%2 == 0 })
+	fmt.Println(t.Items(), f.Items())
+	// Output: [2 4] [1 3]
+}
+
+func ExampleDifferenceMany() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	s2 := FromSlice([]int{1, 2, 3, 4, 5, 6})
+	s3 := FromSlice([]int{6, 7})
+	r := DifferenceMany(s, s2, s3)
+	fmt.Println(r.Items())
+	// Output: [5 7]
+}
+
+func ExampleUnionMany() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	s2 := FromSlice([]int{1, 2, 3, 4, 5, 6})
+	s3 := FromSlice([]int{6, 7})
+	r := UnionMany(s, s2, s3)
+	fmt.Println(r.Items())
+	// Output: [1 2 3 4 5 6 7]
+}
+
+func ExampleIntersectMany() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	s2 := FromSlice([]int{1, 2, 3, 4, 5, 6})
+	s3 := FromSlice([]int{3, 6, 7})
+	r := IntersectMany(s, s2, s3)
+	fmt.Println(r.Items())
+	// Output: [3]
+}
+
+func ExampleMap() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	s2 := Map(func(v int) string { return strconv.Quote(strconv.Itoa(v)) }, s)
+	fmt.Println(s2.Items())
+	// Output: ["1" "2" "3" "4"]
+}
+
+func ExampleMaxElement() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	r := MaxElement(s)
+	fmt.Println(r)
+	// Output: 4
+}
+
+func ExampleMaxElementBy() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	r := MaxElementBy(func(v int) int {
+		if v < 3 {
+			return v * 10
+		}
+		return v
+	}, s)
+	fmt.Println(r)
+	// Output: 2
+}
+
+func ExampleMinElement() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	r := MinElement(s)
+	fmt.Println(r)
+	// Output: 1
+}
+
+func ExampleMinElementBy() {
+	s := FromSlice([]int{1, 2, 3, 4})
+	r := MinElementBy(func(v int) int {
+		if v < 3 {
+			return v * 10
+		}
+		return v
+	}, s)
+	fmt.Println(r)
+	// Output: 3
+}
