54de8c88f1ebc9884e9eaff065d74f1fedc6c0a5
Joel Williams <flowonyx@gmail.com>
2022-03-15 23:51:54 +0200

    Added testing and simplified OrderedMap.

--- orderedMap/map.go
+++ orderedMap/map.go
@@ -1,135 +1,96 @@
+// Package orderedMap offers a map-like type that keeps it's items in either the order they are added or
+// in sorted order if it is provided a less function at creation.
 package orderedMap
 
 import (
-	"sync"
+	"fmt"
 
 	. "github.com/flowonyx/functional"
 	"github.com/flowonyx/functional/errors"
-	. "github.com/flowonyx/functional/list"
+	"github.com/flowonyx/functional/list"
 	"github.com/flowonyx/functional/option"
-	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
 )
 
+// OrderedMap is a map-like structure which keeps items in order.
 type OrderedMap[Key comparable, T any] struct {
 	pairs []Pair[Key, T]
-	keys  map[Key]int
-	mu    *sync.Mutex
-	less  func(Key, Key) bool
+	less  func(Pair[Key, T], Pair[Key, T]) bool
 }
 
-func NewOrderedMap[Key comparable, T any](lessFunc ...func(Key, Key) bool) OrderedMap[Key, T] {
-	var lf func(Key, Key) bool
+// NewOrderedMap creates a new OrderedMap. If lessFunc is provided, it is used to keep the items in sorted order.
+// If lessFunc is not provided, the items are kept in the order in which they are added.
+func NewOrderedMap[Key comparable, T any](lessFunc ...func(Pair[Key, T], Pair[Key, T]) bool) OrderedMap[Key, T] {
+	var lf func(Pair[Key, T], Pair[Key, T]) bool
 	if len(lessFunc) > 0 {
 		lf = lessFunc[0]
 	}
 	return OrderedMap[Key, T]{
 		pairs: make([]Pair[Key, T], 0),
-		keys:  make(map[Key]int),
-		mu:    new(sync.Mutex),
 		less:  lf,
 	}
-
 }
 
-func FromSlice[Key comparable, T any](s []Pair[Key, T], lessFunc ...func(Key, Key) bool) OrderedMap[Key, T] {
-	var lf func(Key, Key) bool
+// FromSlice creates an OrderedMap from a slice of Key, Value Pairs. If lessFunc is provided, the items are sorted.
+// If a key is repeated, the last value for the key is used.
+func FromSlice[Key comparable, T any](s []Pair[Key, T], lessFunc ...func(Pair[Key, T], Pair[Key, T]) bool) OrderedMap[Key, T] {
+	var lf func(Pair[Key, T], Pair[Key, T]) bool
 	if len(lessFunc) > 0 {
 		lf = lessFunc[0]
 	}
-	m := OrderedMap[Key, T]{
-		pairs: slices.Clone(s),
-		keys:  make(map[Key]int),
-		mu:    new(sync.Mutex),
-		less:  lf,
+	// We reverse it to get the latest values for the keys, then reverse it again to get the right order if there is no lessFunc.
+	pairs := list.DistinctBy(func(p Pair[Key, T]) Key { return p.First }, list.Reverse(s))
+	if lf == nil {
+		pairs = list.Reverse(pairs)
+	} else {
+		pairs = list.SortWith(lf, pairs)
 	}
-	if !m.sort() {
-		for i := range m.pairs {
-			key, _ := FromPair(m.pairs[i])
-			m.keys[key] = i
-		}
+	m := OrderedMap[Key, T]{
+		pairs: pairs,
+		less:  lf,
 	}
 	return m
 }
 
-func (m OrderedMap[Key, T]) sort() bool {
-	if m.less != nil {
-		pairs := make([]Pair[Key, T], len(m.pairs))
-		keys := SortWith(m.less, m.Keys())
-		for i, k := range keys {
-			pairs[i] = m.pairs[m.keys[k]]
-			m.keys[pairs[i].First] = i
-		}
-		m.pairs = pairs
-		return true
-	}
-	return false
-}
-
+// ToSlice exports the map as a slice of Key, Value Pairs.
 func (m OrderedMap[Key, T]) ToSlice() []Pair[Key, T] {
-	m.lock()
-	defer m.unlock()
 	return slices.Clone(m.pairs)
 }
 
-func (m OrderedMap[Key, T]) lock() {
-	m.mu.Lock()
-}
-
-func (m OrderedMap[Key, T]) unlock() {
-	m.mu.Unlock()
-}
-
 func (m OrderedMap[Key, T]) clone() OrderedMap[Key, T] {
-	m.lock()
-	defer m.unlock()
-	keys := make(map[Key]int)
-	for k, v := range m.keys {
-		keys[k] = v
-	}
-	return OrderedMap[Key, T]{pairs: slices.Clone(m.pairs), keys: keys, mu: new(sync.Mutex)}
+	return OrderedMap[Key, T]{pairs: slices.Clone(m.pairs)}
 }
 
+// Len returns the length of the map.
 func (m OrderedMap[Key, T]) Len() int {
-	m.lock()
-	defer m.unlock()
 	return len(m.pairs)
 }
 
+// Equal tests whether two OrderedMaps are equal.
+// Equality is based on the keys and values all being the same.
+// Order is not considered.
 func Equal[Key, T comparable](m, m2 OrderedMap[Key, T]) bool {
-	if !maps.Equal(m.keys, m2.keys) {
-		return false
-	}
-	for k := range m.keys {
-		if m2.Get(k) != m.Get(k) {
-			return false
-		}
-	}
-	return true
+	return list.Equal(m.pairs, m2.pairs)
 }
 
-func EqualBy[Key comparable, T any](predicate Predicate2[T, T], m, m2 OrderedMap[Key, T]) bool {
-	if !maps.Equal(m.keys, m2.keys) {
-		return false
-	}
-	for k := range m.keys {
-		if !predicate(m.Get(k), m2.Get(k)) {
-			return false
-		}
-	}
-	return true
+// EqualBy tests whether two OrderedMaps are equal by applying a predicate to each value in both maps.
+func EqualBy[Key comparable, T any](predicate func(T, T) bool, m, m2 OrderedMap[Key, T]) bool {
+	vals1 := list.Map(func(v Pair[Key, T]) T { return v.Second }, m.pairs)
+	vals2 := list.Map(func(v Pair[Key, T]) T { return v.Second }, m2.pairs)
+	return list.ForAll2(predicate, vals1, vals2)
 }
 
+// Contains tests whether the given key is present in the map.
 func (m OrderedMap[Key, T]) Contains(key Key) bool {
-	m.lock()
-	defer m.unlock()
-	_, ok := m.keys[key]
-	return ok
+	return m.indexOf(key) >= 0
 }
 
-func (m OrderedMap[Key, T]) Exists(predicate Predicate2[Key, T]) bool {
-	m.lock()
-	defer m.unlock()
+func (m OrderedMap[Key, T]) indexOf(key Key) int {
+	return list.IndexBy(func(p Pair[Key, T]) bool { return p.First == key }, m.pairs)
+}
+
+// Exists tests whether a key, value pair that matches the predicate is present in the map.
+func (m OrderedMap[Key, T]) Exists(predicate func(Key, T) bool) bool {
 	for _, p := range m.pairs {
 		key, value := FromPair(p)
 		if predicate(key, value) {
@@ -139,54 +100,56 @@ func (m OrderedMap[Key, T]) Exists(predicate Predicate2[Key, T]) bool {
 	return false
 }
 
-func (m OrderedMap[Key, T]) Set(key Key, value T) {
-	m.lock()
-	defer m.unlock()
-	if index, ok := m.keys[key]; ok {
+// Set either adds the key and value to the map or
+// updates the value of the key that is already present.
+func (m *OrderedMap[Key, T]) Set(key Key, value T) {
+	if index := m.indexOf(key); index >= 0 {
 		p := m.pairs[index]
 		p.Second = value
 		m.pairs[index] = p
+		if m.less != nil {
+			list.SortWith(m.less, m.pairs)
+		}
+		return
 	}
 	m.pairs = append(m.pairs, PairOf(key, value))
-	m.keys[key] = len(m.pairs) - 1
-	m.sort()
+	if m.less != nil {
+		list.SortWith(m.less, m.pairs)
+	}
 }
 
+// Get either gets the value associated with the key
+// or returns the zero value of the value type if the
+// key is not present.
 func (m OrderedMap[Key, T]) Get(key Key) T {
-	m.lock()
-	defer m.unlock()
-	if index, ok := m.keys[key]; ok {
+	if index := m.indexOf(key); index >= 0 {
 		return m.pairs[index].Second
 	}
 	return *(new(T))
 }
 
-func (m OrderedMap[Key, T]) Remove(key Key) {
-	m.lock()
-	defer m.unlock()
-	if index, ok := m.keys[key]; ok {
+// Remove removes a key from the map.
+func (m *OrderedMap[Key, T]) Remove(key Key) {
+	if index := m.indexOf(key); index >= 0 {
 		var err error
-		m.pairs, err = RemoveAt(index, m.pairs)
+		m.pairs, err = list.RemoveAt(index, m.pairs)
 		if err != nil {
 			panic(err)
 		}
-		delete(m.keys, key)
-		m.sort()
 	}
 }
 
+// TryGet returns an optional value where if the key exists, it will be Some(value),
+// otherwise it will be None.
 func (m OrderedMap[Key, T]) TryGet(key Key) option.Option[T] {
-	m.lock()
-	defer m.unlock()
-	if index, ok := m.keys[key]; ok {
+	if index := m.indexOf(key); index >= 0 {
 		return option.Some(m.pairs[index].Second)
 	}
 	return option.None[T]()
 }
 
-func (m OrderedMap[Key, T]) Filter(predicate Predicate2[Key, T]) OrderedMap[Key, T] {
-	m.lock()
-	defer m.unlock()
+// Filter returns a new OrderedMap with only the values that match the predicate.
+func (m OrderedMap[Key, T]) Filter(predicate func(Key, T) bool) OrderedMap[Key, T] {
 	output := NewOrderedMap[Key, T]()
 	for _, p := range m.pairs {
 		key, value := FromPair(p)
@@ -197,28 +160,27 @@ func (m OrderedMap[Key, T]) Filter(predicate Predicate2[Key, T]) OrderedMap[Key,
 	return output
 }
 
+// Find is the same as Get except that it returns an error if the key is not found.
 func (m OrderedMap[Key, T]) Find(key Key) (T, error) {
-	m.lock()
-	defer m.unlock()
-	if index, ok := m.keys[key]; ok {
+	if index := m.indexOf(key); index >= 0 {
 		return m.pairs[index].Second, nil
 	}
-	return *(new(T)), errors.KeyNotFoundErr
+	return *(new(T)), fmt.Errorf("OrderedMap.Find(%v): %w", key, errors.KeyNotFoundErr)
 }
 
-func (m OrderedMap[Key, T]) FindKey(predicate Predicate2[Key, T]) (Key, error) {
-	m.lock()
-	defer m.unlock()
+// FindKey returns a key that matches the predicate or a KeyNotFound error.
+func (m OrderedMap[Key, T]) FindKey(predicate func(Key, T) bool) (Key, error) {
 	for _, p := range m.pairs {
 		key, value := FromPair(p)
 		if predicate(key, value) {
 			return key, nil
 		}
 	}
-	return *(new(Key)), errors.KeyNotFoundErr
+	return *(new(Key)), fmt.Errorf("OrderedMap.FindKey: %w", errors.KeyNotFoundErr)
 }
 
-func (m OrderedMap[Key, T]) TryFindKey(predicate Predicate2[Key, T]) option.Option[Key] {
+// TryFindKey is the same as FindKey but it returns an Option with None if no key matches instead of returning an error.
+func (m OrderedMap[Key, T]) TryFindKey(predicate func(Key, T) bool) option.Option[Key] {
 	if value, err := m.FindKey(predicate); err != nil {
 		return option.None[Key]()
 	} else {
@@ -226,9 +188,8 @@ func (m OrderedMap[Key, T]) TryFindKey(predicate Predicate2[Key, T]) option.Opti
 	}
 }
 
-func (m OrderedMap[Key, T]) ForAll(predicate Predicate2[Key, T]) bool {
-	m.lock()
-	defer m.unlock()
+// ForAll tests whether all key, value pairs match the predicate.
+func (m OrderedMap[Key, T]) ForAll(predicate func(Key, T) bool) bool {
 	for _, p := range m.pairs {
 		key, value := FromPair(p)
 		if !predicate(key, value) {
@@ -238,76 +199,84 @@ func (m OrderedMap[Key, T]) ForAll(predicate Predicate2[Key, T]) bool {
 	return true
 }
 
+// IsEmpty tests whether the map is empty.
 func (m OrderedMap[Key, T]) IsEmpty() bool {
 	return len(m.pairs) == 0
 }
 
+// Iter applies the action to each key, value pair in the map.
 func (m OrderedMap[Key, T]) Iter(action func(Key, T)) {
-	m.lock()
-	defer m.unlock()
 	for _, p := range m.pairs {
 		key, value := FromPair(p)
 		action(key, value)
 	}
 }
 
+// Iteri applies the action to each key, value pair in the map, with the index as the first parameter to the action.
 func (m OrderedMap[Key, T]) Iteri(action func(int, Key, T)) {
-	m.lock()
-	defer m.unlock()
 	for i, p := range m.pairs {
 		key, value := FromPair(p)
 		action(i, key, value)
 	}
 }
 
+// Keys returns all the keys in the map.
 func (m OrderedMap[Key, T]) Keys() []Key {
 	output := make([]Key, len(m.pairs))
 	m.Iteri(func(i int, key Key, _ T) { output[i] = key })
 	return output
 }
 
+// Values returns all the values in the map.
 func (m OrderedMap[Key, T]) Values() []T {
 	output := make([]T, len(m.pairs))
 	m.Iteri(func(i int, _ Key, value T) { output[i] = value })
 	return output
 }
 
-func (m OrderedMap[Key, T]) Partition(predicate Predicate2[Key, T]) (OrderedMap[Key, T], OrderedMap[Key, T]) {
-	tmap := NewOrderedMap[Key, T]()
-	fmap := NewOrderedMap[Key, T]()
-	m.lock()
-	defer m.unlock()
+// Partition returns two OrderedMaps where the first contains all key, value pairs that match the predicate
+// and the second contains all key, value pairs that do not match the predicate.
+func (m OrderedMap[Key, T]) Partition(predicate func(Key, T) bool) (trueMap OrderedMap[Key, T], falseMap OrderedMap[Key, T]) {
+	trueMap = NewOrderedMap[Key, T]()
+	falseMap = NewOrderedMap[Key, T]()
 	for _, p := range m.pairs {
 		key, value := FromPair(p)
 		if predicate(key, value) {
-			tmap.Set(key, value)
+			trueMap.Set(key, value)
 		} else {
-			fmap.Set(key, value)
+			falseMap.Set(key, value)
 		}
 	}
-	return tmap, fmap
+	return trueMap, falseMap
 }
 
+// Fold applies the folder function to each key, value pair until arriving at the final state.
 func Fold[Key comparable, T, State any](folder func(State, Key, T) State, initial State, table OrderedMap[Key, T]) State {
-	table.lock()
-	defer table.unlock()
-	return Fold(func(s State, t Pair[Key, T]) State {
+	return list.Fold(func(s State, t Pair[Key, T]) State {
 		return folder(s, t.First, t.Second)
 	}, initial, table.pairs)
 }
 
+// FoldBack applies the folder function in reverse order to each key, value pair until arriving at the final state.
 func FoldBack[Key comparable, T, State any](folder func(Key, T, State) State, table OrderedMap[Key, T], initial State) State {
-	table.lock()
-	defer table.unlock()
-	return FoldBack(func(t Pair[Key, T], s State) State {
+	return list.FoldBack(func(t Pair[Key, T], s State) State {
 		return folder(t.First, t.Second, s)
 	}, table.pairs, initial)
 }
 
-func MapTo[Key comparable, T, R any](mapping func(Key, T) R, table OrderedMap[Key, T]) OrderedMap[Key, R] {
+// MapTo creates a new map with the keys and values changed through the mapping function.
+func MapTo[Key, KeyR comparable, T, ValueR any](mapping func(Key, T) (KeyR, ValueR), table OrderedMap[Key, T]) OrderedMap[KeyR, ValueR] {
+	output := NewOrderedMap[KeyR, ValueR]()
+	for _, p := range table.pairs {
+		key, value := FromPair(p)
+		output.Set(mapping(key, value))
+	}
+	return output
+}
+
+// MapValuesTo creates a new map with the same keys, but the values are changed through the mapping function.
+func MapValuesTo[Key comparable, T, R any](mapping func(Key, T) R, table OrderedMap[Key, T]) OrderedMap[Key, R] {
 	output := NewOrderedMap[Key, R]()
-	table.lock()
-	defer table.unlock()
 	for _, p := range table.pairs {
 		key, value := FromPair(p)
 		output.Set(key, mapping(key, value))
@@ -315,6 +284,7 @@ func MapTo[Key comparable, T, R any](mapping func(Key, T) R, table OrderedMap[Ke
 	return output
 }
 
+// Pick searches the map looking for the first element where the given function returns a Some value. Returns a KeyNotFoundErr if no such element exists.
 func Pick[Key comparable, T, R any](chooser func(Key, T) option.Option[R], table OrderedMap[Key, T]) (R, error) {
 	for _, p := range table.pairs {
 		key, value := FromPair(p)
@@ -323,9 +293,10 @@ func Pick[Key comparable, T, R any](chooser func(Key, T) option.Option[R], table
 			return v.Value(), nil
 		}
 	}
-	return *(new(R)), errors.KeyNotFoundErr
+	return *(new(R)), fmt.Errorf("Pick: %w", errors.KeyNotFoundErr)
 }
 
+// TryPick searches the map looking for the first element where the given function returns a Some value and returns the Some value. Returns None if no such element exists.
 func TryPick[Key comparable, T, R any](chooser func(Key, T) option.Option[R], table OrderedMap[Key, T]) option.Option[R] {
 	for _, p := range table.pairs {
 		key, value := FromPair(p)
@@ -337,12 +308,14 @@ func TryPick[Key comparable, T, R any](chooser func(Key, T) option.Option[R], ta
 	return option.None[R]()
 }
 
+// Set returns a copy of table with the key set the value.
 func Set[Key comparable, T any](table OrderedMap[Key, T], key Key, value T) OrderedMap[Key, T] {
 	m := table.clone()
 	m.Set(key, value)
 	return m
 }
 
+// Remove returns a copy of table with the key removed.
 func Remove[Key comparable, T any](table OrderedMap[Key, T], key Key) OrderedMap[Key, T] {
 	m := table.clone()
 	m.Remove(key)
---
+++ orderedMap/map_test.go
@@ -0,0 +1,338 @@
+package orderedMap
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/flowonyx/functional"
+	"github.com/flowonyx/functional/errors"
+	"github.com/flowonyx/functional/option"
+)
+
+func ExampleOrderedMap_Set() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(1, "one")
+	m.Set(2, "two")
+	fmt.Println(m.Keys(), m.Values())
+	// Output: [1 2] [one two]
+}
+
+func ExampleFromSlice() {
+	m := FromSlice([]functional.Pair[string, int]{
+		functional.PairOf("one", 1),
+		functional.PairOf("two", 2),
+		functional.PairOf("two", 3),
+	})
+
+	fmt.Println(m.Keys(), m.Values())
+	// Output: [one two] [1 3]
+}
+
+func ExampleEqual() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+	m2 := NewOrderedMap[int, string]()
+	m2.Set(1, "one")
+	m2.Set(2, "two")
+	m3 := NewOrderedMap[int, string]()
+	m3.Set(1, "one")
+	m3.Set(2, "two")
+	m3.Set(3, "three")
+	m4 := NewOrderedMap[int, string]()
+	m4.Set(1, "One")
+	m4.Set(2, "Two")
+	fmt.Println(Equal(m, m2), Equal(m, m3), Equal(m, m4))
+	// Output: true false false
+}
+
+func ExampleEqualBy() {
+	type vt struct {
+		Value string
+	}
+	m := NewOrderedMap[int, vt]()
+	m.Set(1, vt{Value: "one"})
+	m.Set(2, vt{Value: "two"})
+	m.Set(3, vt{Value: "three"})
+
+	m2 := NewOrderedMap[int, vt]()
+	m2.Set(1, vt{Value: "one"})
+	m2.Set(2, vt{Value: "two"})
+	m2.Set(3, vt{Value: "three"})
+
+	m3 := NewOrderedMap[int, vt]()
+	m3.Set(1, vt{Value: "One"})
+	m3.Set(2, vt{Value: "Two"})
+	m3.Set(3, vt{Value: "Three"})
+
+	pred := func(v1 vt, v2 vt) bool {
+		return v1.Value == v2.Value
+	}
+
+	fmt.Println(EqualBy(pred, m, m2), EqualBy(pred, m, m3))
+	// Output: true false
+}
+
+func ExampleOrderedMap_Get() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+
+	fmt.Println(m.Get(1))
+	// Output: one
+}
+
+func ExampleOrderedMap_Remove() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+
+	m.Remove(1)
+
+	fmt.Println(m.TryGet(1))
+	// Output: None
+}
+
+func ExampleOrderedMap_Filter() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+	m.Set(3, "Three")
+
+	r := m.Filter(func(i int, s string) bool { return strings.HasPrefix(s, "T") })
+	fmt.Println(r.Keys())
+	// Output: [3]
+}
+
+func ExampleOrderedMap_Find() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+
+	v, err := m.Find(1)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = m.Find(3)
+
+	fmt.Println(v, errors.Is(err, errors.KeyNotFoundErr))
+	// Output: one true
+}
+
+func ExampleOrderedMap_FindKey() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+
+	v, err := m.FindKey(func(key int, value string) bool { return strings.HasPrefix(value, "t") })
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = m.FindKey(func(key int, value string) bool { return strings.HasPrefix(value, "T") })
+
+	fmt.Println(v, errors.Is(err, errors.KeyNotFoundErr))
+	// Output: 2 true
+}
+
+func ExampleOrderedMap_TryFindKey() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+
+	v := m.TryFindKey(func(_ int, value string) bool { return strings.HasPrefix(value, "t") })
+	v2 := m.TryFindKey(func(_ int, value string) bool { return strings.HasPrefix(value, "T") })
+
+	fmt.Println(v, v2)
+	// Output: Some(2) None
+}
+
+func ExampleOrderedMap_ForAll() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+
+	lessThan3 := func(key int, _ string) bool { return key < 3 }
+
+	r1 := m.ForAll(lessThan3)
+	m.Set(3, "three")
+	r2 := m.ForAll(lessThan3)
+	fmt.Println(r1, r2)
+	// Output: true false
+}
+
+func ExampleOrderedMap_Iter() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+
+	m.Iter(func(key int, value string) {
+		fmt.Print(key, ":", value, ",")
+	})
+
+	// Output: 1:one,2:two,
+}
+
+func ExampleOrderedMap_Iteri() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+
+	m.Iteri(func(i int, key int, value string) {
+		fmt.Print(i, ":", key, ":", value, ",")
+	})
+
+	// Output: 0:1:one,1:2:two,
+}
+
+func ExampleOrderedMap_Keys() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+
+	fmt.Println(m.Keys())
+	// Output: [1 2]
+}
+
+func ExampleOrderedMap_Values() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+
+	fmt.Println(m.Values())
+	// Output: [one two]
+}
+
+func ExampleOrderedMap_Partition() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+	m.Set(3, "three")
+
+	t, f := m.Partition(func(i int, _ string) bool {
+		return i < 3
+	})
+
+	fmt.Println(t.Contains(3), f.Contains(3))
+	// Output: false true
+}
+
+func ExampleFold() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+	m.Set(3, "three")
+
+	r := Fold(func(state int, key int, value string) int {
+		// (((((1 * 1) + 1) * 2) +1) * 3) = 15
+		return (state + 1) * key
+	}, 0, m)
+
+	fmt.Println(r)
+	// Output: 15
+}
+
+func ExampleFoldBack() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+	m.Set(3, "three")
+
+	r := FoldBack(func(key int, value string, state int) int {
+		// (((((1 * 3) + 1) * 2) +1) * 1) = 9
+		return (state + 1) * key
+	}, m, 0)
+
+	fmt.Println(r)
+	// Output: 9
+}
+
+func ExampleMapTo() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+	m.Set(3, "three")
+
+	r := MapTo(func(key int, value string) (string, int) {
+		return value, key
+	}, m)
+
+	fmt.Println(r.Keys())
+	// Output: [one two three]
+}
+
+func ExampleMapValuesTo() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+	m.Set(3, "three")
+
+	r := MapValuesTo(func(key int, value string) int {
+		return key + 1
+	}, m)
+
+	fmt.Println(r.Get(1))
+	// Output: 2
+}
+
+func ExamplePick() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+	m.Set(3, "three")
+
+	r, err := Pick(func(key int, _ string) option.Option[float64] {
+		if key > 1 {
+			return option.Some(float64(key))
+		}
+		return option.None[float64]()
+	}, m)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%.2f", r)
+	// Output: 2.00
+}
+
+func ExampleTryPick() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+	m.Set(3, "three")
+
+	r := TryPick(func(key int, _ string) option.Option[float64] {
+		if key > 1 {
+			return option.Some(float64(key))
+		}
+		return option.None[float64]()
+	}, m)
+
+	fmt.Println(r)
+	// Output: Some(2)
+}
+
+func ExampleSet() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+	m.Set(3, "three")
+
+	r := Set(m, 2, "Two")
+	fmt.Println(r.Values())
+	// Output: [one Two three]
+}
+
+func ExampleRemove() {
+	m := NewOrderedMap[int, string]()
+	m.Set(1, "one")
+	m.Set(2, "two")
+	m.Set(3, "three")
+
+	r := Remove(m, 2)
+	fmt.Println(r.Keys())
+	// Output: [1 3]
+}
