862592757bf9097021447f75a5f45de32e57fb49
Joel Williams <flowonyx@gmail.com>
2022-03-27 21:53:48 +0200

    Fixed a bug

--- list/fill.go
+++ list/fill.go
@@ -10,10 +10,10 @@ import (
 // If startIndex is outside the range of indexes in values, it will return an IndexOutOfRangeErr.
 // If count goes beyond the end of values, it flils the end of values.
 func Fill[T any](values []T, startIndex int, count int, value T) error {
-	if len(values) < 0 || len(values) < startIndex+count {
+	if len(values) == 0 || len(values) < startIndex+count {
 		return fmt.Errorf("%w: Fill([%d]%T, %d, %d, _)", errors.IndexOutOfRangeErr, len(values), value, startIndex, count)
 	}
-	count = Min(count, len(values[startIndex:]))
+	count, _ = Min(count, len(values[startIndex:]))
 
 	for _, i := range Range(startIndex, startIndex+count-1) {
 		values[i] = value
