6bdc34da78f1bf42ba1d55cb87b057b659af6521
Joel Williams <flowonyx@gmail.com>
2022-03-27 21:22:01 +0200

    Simplified the code for Sum and SumBy

--- list/sum.go
+++ list/sum.go
@@ -2,10 +2,18 @@ package list
 
 // Sum returns the result of adding all values together.
 func Sum[T numeric | ~string](values []T) T {
-	return Reduce(*(new(T)), func(accumulator, each T) T { return accumulator + each }, values)
+	var r T
+	for _, v := range values {
+		r += v
+	}
+	return r
 }
 
 // SumBy returns the result of adding all results of applying projection to each value.
 func SumBy[T any, R numeric | ~string](projection func(T) R, values []T) R {
-	return Reduce(*(new(R)), func(accumulator R, each T) R { return accumulator + projection(each) }, values)
+	var r R
+	for _, v := range values {
+		r += projection(v)
+	}
+	return r
 }
